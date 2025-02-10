641eca25be0a54a85c5a9c460c7b704119c15fc3
Mohanraj38659 <161623451+Mohanraj38659@users.noreply.github.com>
2025-02-10 07:57:41 -0800

    Create server.js

---
+++ client/client/server/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const Todo = require('./models/todo');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+// Routes
+app.get('/api/todos', async (req, res) => {
+    try {
+        const todos = await Todo.find();
+        res.json(todos);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+app.post('/api/todos', async (req, res) => {
+    const todo = new Todo({
+        task: req.body.task
+    });
+    try {
+        const newTodo = await todo.save();
+        res.status(201).json(newTodo);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
